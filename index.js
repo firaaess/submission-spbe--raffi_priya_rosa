f99b3314c4e0a399f645e5dcf80667f23d44b371
firaaess <plantongaming24@gmail.com>
2025-05-13 19:07:31 +0700

    fixing eror in vercel

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 // Routes untuk Auth
 app.use('/api', routes);
 
-app.listen(9090, () => {
-  console.log(`Server is running on http://localhost:9090`);
-});
-  
+// app.listen(9090, () => {
+//   console.log(`Server is running on http://localhost:9090`);
+// });
+
+export default app;
