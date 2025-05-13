a8f058c534da6f94967b5ea1ea3ce1c68a45f9bd
firaaess <plantongaming24@gmail.com>
2025-05-13 19:31:38 +0700

    delete vercel

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(express.json());
 // Routes untuk Auth
 app.use('/api', routes);
 
-// app.listen(9090, () => {
-//   console.log(`Server is running on http://localhost:9090`);
-// });
-
-export default app;
+app.listen(9090, () => {
+  console.log(`Server is running on http://localhost:9090`);
+});
